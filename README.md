09a58d236b5198afeeea3ac181de3d1a12dbe5f9
gluay88 <p.wantagaew@gmail.comm>
2022-10-06 15:29:49 -0600

    added useEffect JSON api

--- README.md
+++ README.md
@@ -2,4 +2,24 @@
 
 - I was in bed for over 20 hours
 - Amazon Product is just sth. for fun to kill time..
-- I'm not sure how much I get done here.. I'll take some meds and they will probably knock me out real soon...
+- I'm not sure how much I'll get done here.. I'll take some meds and they will probably knock me out real soon...
+
+\*\* addEventListener
+
+- Here are some of the most common event types and event names:
+- 1. Mouse Events: click, dblclick, mousedown, mouseup, contextmenu, mouseout, mousewheel, mouseover
+- 2. Touch Events: touchstart, touchend, touchmove, touchcancel
+- 3. Keyboard Events: keydown, keyup, keypress
+- 4. Form Events: focus, blur, change, submit
+- 5. Window Events: resize, scroll, load, unload, hashchange
+
+- use return a function and removeEventListener
+
+```
+useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+```
--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Product from "./Product";
 import "./App.css";
 
 function App() {
+  // useEffect stuff..
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  // useState stuff..
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  // useEffect for the jsonplaceholder..
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
+
+  // useEffect for the windowInnerWidth..
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  // addEventListener(type, listener, options)
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    //clean up whatever we did last time..
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="App">
       <h1>Amazon Products</h1>
@@ -21,6 +55,29 @@ function App() {
         description="Your favorite Computer"
         price={2500}
       />
+      <br />
+      <div>
+        <h2>Counter app - useState</h2>
+        <p>The count is: {count}</p>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </div>
+      <br />
+      <div>
+        <h2>useEffect()</h2>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+        <div>ResourceType: {resourceType}</div>
+        {items.map((item) => {
+          return <pre key={item.index}>{JSON.stringify(item)}</pre>;
+        })}
+      </div>
+      <br />
+      <div>
+        <h2>WindowInnerWidth - addEventListener</h2>
+        {windowWidth}
+      </div>
     </div>
   );
 }
